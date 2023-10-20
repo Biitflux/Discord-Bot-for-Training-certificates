420d5b0aa2fbea66ad9363c82e11899bff82e20f
Biitflux <nikolaimaxi.walkenfort@gmail.com>
2023-10-20 09:03:40 +0200

    update

--- sources/infos.py
+++ sources/infos.py
@@ -12,6 +12,6 @@ def_picture = "https://i.imgur.com/OJKP3T4.png"
 bot_picture = "https://i.imgur.com/YzU0Lp7.png"
 clock_picture = "https://i.imgur.com/ksNPOwe.png"
 
-BOT_TOKEN ="MTEyNTMwMDkxNjQyMjE5MzE5NA.GpILdy.eZeEx0p-qCUnZZp2n8GxpmkFHGUeb2OdV7_wbs"
+BOT_TOKEN ="BOT_TOKEN"
 bot_version = "Alpha 1.0.0"
 bot_developer = "Bitflux_"
